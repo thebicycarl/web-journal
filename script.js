a092c23f595d2a35fa256d18c9ddc4c1005c3142
CarlHockey <thebicycarl@gmail.com>
2024-02-21 11:56:29 +1100

    Built basic input function

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// First could be to make a pop-up for the menu button to select a category
+const mainInput = document.getElementById('mainInput');
+const entriesSection = document.getElementById('entries');
+const addEntry = document.getElementById('addEntry');
+
+addEntry.addEventListener("click", postEntry);
+
+function postEntry() {
+    // Assign todays date to variable entryDate in format: dd mmm
+    const today = new Date();
+    const dd = today.getDate().toString().padStart(2, '0');
+    const mmm = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][today.getMonth()];
+
+    const entryDate = `${dd} ${mmm}`;
+
+    const innerEntry = `
+    <div>
+        <p class="date">${entryDate}</p>
+    </div>
+    <div>
+        <p>${mainInput.value}</p>
+    </div>
+    `;
+
+    const newEntry = document.createElement('div');
+    newEntry.setAttribute('class', 'addedEntry')
+    newEntry.innerHTML = innerEntry;
+
+    entriesSection.insertBefore(newEntry, entriesSection.firstChild);
+
+    mainInput.value = "";
+
+
+}
